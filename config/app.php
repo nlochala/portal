de7a173a4c38a0db509aa182b55e5aa6bcdb4854
Nathan Lochala <rnlochala@alaska.edu>
2019-05-13 12:05:35 +0800

    install laravel/telescope

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+//        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
 
@@ -221,7 +222,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Helpers' => App\Helpers::class,
+        'Helpers' => App\Helpers\Helpers::class,
         'Image' => Image::class,
     ],
 ];
